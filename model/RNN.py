f9f9e48cd7462d269c9f6bcafe737cc3898e04c4
irobbwu <74640408+irobbwu@users.noreply.github.com>
2022-06-14 15:30:06 +0800

    Add files via upload

--- model/RNN.py
+++ model/RNN.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from string import punctuation
 from torch import nn
+import math
 
 
 def txt_prepocessing(txt_path):
@@ -234,3 +235,169 @@ def train_rnn(net, epochs, train_iter, loss, optim,
     plt.ylabel('perplexity')
     plt.legend()
     plt.show()
+
+
+# additive attention
+class AdditiveAttention(nn.Module):
+  def __init__(self, key_size, query_size, num_hiddens, dropout, **kwargs):
+    super(AdditiveAttention, self).__init__(**kwargs)
+    self.W_k = nn.Linear(key_size, num_hiddens, bias=False)
+    self.W_q = nn.Linear(query_size, num_hiddens, bias=False)
+    self.w_v = nn.Linear(num_hiddens, 1, bias=False)
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, queries, keys, values, valid_lens):
+    queries, keys = self.W_q(queries), self.W_k(keys)
+    features = queries.unsqueeze(2) + keys.unsqueeze(1)
+    features = torch.tanh(features)
+    scores = self.w_v(features).squeeze(-1)
+    self.attention_weights = masked_softmax(scores, valid_lens)
+    # values的形状：(batch_size，“键－值”对的个数，值的维度)
+    return torch.bmm(self.dropout(self.attention_weights), values)
+
+
+def sequence_mask(X, valid_len, value=0):
+  """在序列中屏蔽不相关的项"""
+  maxlen = X.size(1)
+  mask = torch.arange((maxlen), dtype=torch.float32,
+          device=X.device)[None, :] < valid_len[:, None]
+  X[~mask] = value
+  return X
+
+class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
+  """带遮蔽的softmax交叉熵损失函数"""
+  # pred的形状：(batch_size,num_steps,vocab_size)
+  # label的形状：(batch_size,num_steps)
+  # valid_len的形状：(batch_size,)
+  def forward(self, pred, label, valid_len):
+    weights = torch.ones_like(label)
+    weights = sequence_mask(weights, valid_len)
+    self.reduction='none'
+    unweighted_loss = super().forward(
+                pred.permute(0, 2, 1), label)
+    weighted_loss = (unweighted_loss * weights).mean(dim=1)
+    return weighted_loss
+
+def masked_softmax(X, valid_lens):
+  """通过在最后⼀个轴上掩蔽元素来执⾏softmax操作"""
+  # X:3D张量，valid_lens:1D或2D张量
+  if valid_lens is None:
+    return nn.functional.softmax(X, dim=-1)
+  else:
+    shape = X.shape
+  if valid_lens.dim() == 1:
+    valid_lens = torch.repeat_interleave(valid_lens, shape[1])
+  else:
+    valid_lens = valid_lens.reshape(-1)
+  # 最后⼀轴上被掩蔽的元素使⽤⼀个⾮常⼤的负值替换，从⽽其softmax输出为0
+  X = sequence_mask(X.reshape(-1, shape[-1]), valid_lens,
+          value=-1e6)
+  return nn.functional.softmax(X.reshape(shape), dim=-1)
+
+class Seq2SeqEncoder(nn.Module):
+  def __init__(self, vocab_size, embed_size, num_hiddens, num_layers,
+               dropout=0, **kwargs):
+    super().__init__(**kwargs)
+    # embedding
+    self.embedding = nn.Embedding(vocab_size, embed_size)
+    self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, dropout=dropout)
+
+  def forward(self, X, *args):
+    # 输出'X'的形状：(batch_size,num_steps,embed_size)
+    X = self.embedding(X)
+    X = X.permute(1, 0, 2)
+    output, state = self.rnn(X)
+    return output, state
+
+class EncoderDecoder(nn.Module):
+  """编码器-解码器架构的基类"""
+  def __init__(self, encoder, decoder, **kwargs):
+    super().__init__(**kwargs)
+    self.encoder = encoder
+    self.decoder = decoder
+
+  def forward(self, enc_X, dec_X, *args):
+    enc_outputs = self.encoder(enc_X, *args)
+    dec_state = self.decoder.init_state(enc_outputs, *args)
+    return self.decoder(dec_X, dec_state)
+
+def train_seq2seq(net, epochs, train_iter, loss, optim, tgt_vocab,
+  grad_clip_theta=1, device=torch.device('cuda')):
+  net.to(device)
+  net.train()
+
+  loss_fig = []
+  for epoch in range(epochs):
+    epoch_loss = []
+    for batch in train_iter:
+      optim.zero_grad()
+      X, X_valid_len, Y, Y_valid_len = [x.to(device) for x in batch]
+      bos = torch.tensor([tgt_vocab['<bos>']] * Y.shape[0],
+                          device=device).reshape(-1, 1)
+      dec_input = torch.cat([bos, Y[:, :-1]], 1) # 强制教学
+      Y_hat, _ = net(X, dec_input, X_valid_len)
+      l = loss(Y_hat, Y, Y_valid_len)
+      epoch_loss.append(l.mean().item())
+      l.sum().backward()
+      grad_clipping(net, grad_clip_theta)
+      optim.step()
+    epoch_perplexity = np.mean(epoch_loss)
+    if (epoch+1)%50==0:
+        print(f'epoch: {epoch+1}, loss:{epoch_perplexity:f}')
+    loss_fig.append(epoch_perplexity)
+
+  plt.plot(loss_fig, label='train', color='b', linestyle='solid')
+  plt.xlabel('epoch')
+  plt.ylabel('loss')
+  plt.legend()
+  plt.show()
+
+def predict_seq2seq(net, src_sentence, src_vocab, tgt_vocab, num_steps,
+                    device=torch.device('cuda'), save_attention_weights=False):
+    net.eval()
+    src_tokens = src_vocab[src_sentence.lower().split(' ')] + [
+                            src_vocab['<eos>']]
+    enc_valid_len = torch.tensor([len(src_tokens)], device=device)
+    src_tokens = truncate_pad(src_tokens, num_steps, src_vocab['<pad>'])
+    enc_X = torch.unsqueeze(
+        torch.tensor(src_tokens, dtype=torch.long, device=device), dim=0)
+    enc_outputs = net.encoder(enc_X, enc_valid_len)
+    dec_state = net.decoder.init_state(enc_outputs, enc_valid_len)
+    dec_X = torch.unsqueeze(torch.tensor(
+                [tgt_vocab['<bos>']], dtype=torch.long, device=device), dim=0)
+    output_seq, attention_weight_seq = [], []
+    for _ in range(num_steps):
+        Y, dec_state = net.decoder(dec_X, dec_state)
+        # 我们使⽤具有预测最⾼可能性的词元，作为解码器在下⼀时间步的输⼊
+        dec_X = Y.argmax(dim=2)
+        pred = dec_X.squeeze(dim=0).type(torch.int32).item()
+        if save_attention_weights:
+            attention_weight_seq.append(net.decoder.attention_weights)
+        # ⼀旦序列结束词元被预测，输出序列的⽣成就完成了
+        if pred == tgt_vocab['<eos>']:
+            break
+        output_seq.append(pred)
+    return ' '.join(tgt_vocab.to_tokens(output_seq)), attention_weight_seq
+
+
+def bleu(pred_seq, label_seq, k): #@save
+    """计算BLEU"""
+    pred_tokens, label_tokens = pred_seq.split(' '), label_seq.split(' ')
+    len_pred, len_label = len(pred_tokens), len(label_tokens)
+    score = math.exp(min(0, 1 - len_label / len_pred))
+    for n in range(1, k + 1):
+        num_matches, label_subs = 0, collections.defaultdict(int)
+        for i in range(len_label - n + 1):
+            label_subs[' '.join(label_tokens[i: i + n])] += 1
+        for i in range(len_pred - n + 1):
+            if label_subs[' '.join(pred_tokens[i: i + n])] > 0:
+                num_matches += 1
+                label_subs[' '.join(pred_tokens[i: i + n])] -= 1
+        score *= math.pow(num_matches / (len_pred - n + 1), math.pow(0.5, n))
+    return score
+
+def truncate_pad(line, num_steps, padding_token):
+  """截断或填充⽂本序列"""
+  if len(line) > num_steps:
+    return line[:num_steps] # 截断
+  return line + [padding_token] * (num_steps - len(line)) # 填充
