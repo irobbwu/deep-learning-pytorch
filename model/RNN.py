19ebbd9412cd67c231cefd3623281ac922c2852f
irobbwu <74640408+irobbwu@users.noreply.github.com>
2022-06-19 17:59:06 +0800

    Add files via upload

--- model/RNN.py
+++ model/RNN.py
@@ -401,3 +401,21 @@ def truncate_pad(line, num_steps, padding_token):
   if len(line) > num_steps:
     return line[:num_steps] # 截断
   return line + [padding_token] * (num_steps - len(line)) # 填充
+
+
+class DotProductAttention(nn.Module):
+  """缩放点积注意⼒"""
+  def __init__(self, dropout, **kwargs):
+    super(DotProductAttention, self).__init__(**kwargs)
+    self.dropout = nn.Dropout(dropout)
+    # queries的形状：(batch_size，查询的个数，d)
+    # keys的形状：(batch_size，“键－值”对的个数，d)
+    # values的形状：(batch_size，“键－值”对的个数，值的维度)
+    # valid_lens的形状:(batch_size，)或者(batch_size，查询的个数)
+
+  def forward(self, queries, keys, values, valid_lens=None):
+    d = queries.shape[-1]
+    # 设置transpose_b=True为了交换keys的最后两个维度
+    scores = torch.bmm(queries, keys.transpose(1,2)) / math.sqrt(d)
+    self.attention_weights = masked_softmax(scores, valid_lens)
+    return torch.bmm(self.dropout(self.attention_weights), values)
